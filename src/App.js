d02a6569a9e21f1df901c32c71c6163f75a9b5ce
José <josevictortf@gmail.com>
2021-03-01 15:23:18 -0300

    Tirando do app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import { CategoryFilter } from './Components/CategoryFilter/CategoryFilter';
-import { SearchFilter } from './Components/SearchFilter/SearchFilter';
 
 function App() {
   return (
     <div>
       <h1>Future Eats</h1> 
-      <SearchFilter />
-      <CategoryFilter />
     </div>
   );
 }
