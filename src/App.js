b4895575d5b008dfc52287f4227d128f2bf6fce4
José <josevictortf@gmail.com>
2021-03-01 14:32:52 -0300

    Filter component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { CategoryFilter } from './Components/CategoryFilter/CategoryFilter';
+import { SearchFilter } from './Components/SearchFilter/SearchFilter';
 
 function App() {
   return (
     <div>
-      <h1>Futere Eats</h1>  
+      <h1>Future Eats</h1> 
+      <SearchFilter />
+      <CategoryFilter />
     </div>
   );
 }
---
+++ src/Components/CategoryFilter/styled.js
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+export const FilterField = styled.div`
+  width: 100vw;
+  max-width: 100%;
+  padding: 12px 0 12px 16px;
+  display: flex;
+  flex-direction: wrap;
+  align-items: center;
+  margin: 8px 0 0;
+  overflow-x: scroll;
+  ::-webkit-scrollbar {
+    height: 10px;
+    width: 4px;
+    border: 1px solid transparent;
+  }
+`;
+
+export const FilterText = styled.div`
+  width: 100vw;
+  max-width: 420px;
+  height: 15px;
+  display: flex;
+  flex-direction: wrap;
+  :first-child {
+    margin-left: 18px;
+  }
+
+  p {
+    cursor: pointer;
+    width: 87px;
+    height: 18px;
+    margin: 0 8px 0 0;
+    padding: 0 0 12px 29px;
+    font-size: 16px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: -0.39px;
+    text-align: center;
+
+    &:hover,
+    :active {
+      color: var(--mid-green);
+    }
+  }
+`;
