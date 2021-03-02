ccd4d1d2633501a484d2fe64eff7cde671da2de8
Bruno-Galhardi <brunogvallim@gmail.com>
2021-03-02 16:59:25 -0300

    Pagina de login pronta e estilizada, falta bater na API, criado um useform dentro da pasta hooks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import LoginPage from './Screens/LoginPage/LoginPage';
 
 function App() {
   return (
     <div>
       <h1>Future Eats</h1>
-      <div>
-        <LoginPage/>
-      </div>
-       
     </div>
   );
 }
