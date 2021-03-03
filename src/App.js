4ee5a62cc0fa32c740d9a51876fc63d1cb2253f4
fsantos22 <22fsantos@gmail.com>
2021-03-03 01:01:56 -0300

    Screen de restaurantes finalizada + alterações no cart card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Restaurants from './Screens/Restaurants';
 
 function App() {
   return (
     <div>
-      <h1>Future Eats</h1> 
+      {/* <h1>Future Eats</h1>  */}
+      <Restaurants/>
     </div>
   );
 }
--- src/Components/CartCard/index.js
+++ src/Components/CartCard/index.js
@@ -1,30 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "./styled";
+import Popup from "./Popup";
 
 const CartCard = (props) => {
+  const [quantity, setQuantity] = useState(0);
+  const [openPopup, setOpenPopup] = useState(false);
+  const addItem = () => {
+    quantity ? setQuantity(0) : setOpenPopup(true);
+  };
+
   return (
-    <Card>
-      <div id="Retangle">
-        <div id="Image">
-          <img src={props.img} />
-        </div>
-        <div id="Content">
-          <div id="Header">
-            <p id="Title">{props.title}</p>
-            <div id="Quantity">
-              <p>{props.quantity}</p>
+    <>
+      {openPopup && (
+        <Popup setQuantity={setQuantity} setOpenPopup={setOpenPopup} />
+      )}
+      <Card quantity={quantity}>
+        <div id="Rectangle">
+          <div id="Image">
+            <img src={props.img} />
+          </div>
+          <div id="Content">
+            <div id="Header">
+              <p id="Title">{props.title}</p>
+              {quantity ? (
+                <div id="Quantity">
+                  <p>{quantity}</p>
+                </div>
+              ) : null}
+            </div>
+            <div id="Main">
+              <p>{props.description}</p>
+            </div>
+            <div id="Footer">
+              <p>R${props.price}</p>
+              <button id="Btn" onClick={addItem}>{quantity ? "remover" : "adicionar"} </button>
             </div>
           </div>
-          <div id="Main">
-            <p>{props.description}</p>
-          </div>
-          <div id="Footer">
-            <p>R${props.price}</p>
-            <button id="remove-btn">remover</button>
-          </div>
         </div>
-      </div>
-    </Card>
+      </Card>
+    </>
   );
 };
 
