d3d7051e00efec04b5fbe19fd2b301513bf8ce8c
shidoshi93 <dann.ribeiroo@gmail.com>
2021-03-05 15:23:43 +0000

    Terminado rotas.

--- src/Routes/Router.js
+++ src/Routes/Router.js
@@ -2,13 +2,47 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import ErrorPage from "../Screens/ErrorPage/ErrorPage";
 import FeedPage from "../Screens/FeedPage/FeedPage";
+import LoginPage from '../Screens/LoginPage/LoginPage'
+import RegisterPage from "../Screens/RegisterPage/RegisterPage";
+import AddressPage from '../Screens/AdressForm/AdressPage';
+import RestarantsInfos from '../Screens/Restaurants/index';
+import ProfilePage from '../Screens/ProfilePage/ProfilePage';
+import EditAddress from '../Screens/ProfilePage/EditPage/EditAddress';
+import EdditInfos from '../Screens/ProfilePage/EditPage/EditInfo';
 
 function Router() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route exact path='/restaurantes'>
+                <Route exact path='/feed'>
                     <FeedPage />
+                </Route>
+
+                <Route exact path='/'>
+                    <LoginPage />
+                </Route>
+
+                <Route exact path='/register'>
+                    <RegisterPage />
+                </Route>
+
+                <Route exact path='/address-form'>
+                    <AddressPage />
+                </Route>
+
+                <Route exact path='/restaurants-infos'>
+                    <RestarantsInfos />
+                </Route>
+
+                <Route exact path='/profile'>
+                    <ProfilePage />
+                </Route>
+                <Route exact path='/edit-profile-address'>
+                    <EditAddress />
+                </Route>
+
+                <Route exact path='/edit-profile-info'>
+                    <EdditInfos />
                 </Route>
 
                 <Route>
--- src/Screens/LoginPage/LoginPage.js
+++ src/Screens/LoginPage/LoginPage.js
@@ -2,17 +2,20 @@ import React from 'react'
 import LoginForm from './LoginForm'
 import Logo from '../../Imgs/logo-future-eats-invert.png'
 import { LogoImg, TitleEnter, TitleRegister } from './styled'
+import { useHistory } from 'react-router'
+import { goToRegister, goToFeed } from '../../Routes/Coordinator'
 
 const LoginPage = () =>{
+     const history = useHistory()
     return(
         <div>
             <LogoImg src={Logo} />
             <TitleEnter>
-                <p>Entrar</p>
+                <p onClick={() => goToFeed(history)}>Entrar</p>
             </TitleEnter>
             <LoginForm/>
             <TitleRegister>
-                <p>Não possui cadastro? Clique aqui.</p>
+                <p onClick={() => goToRegister(history)}>Não possui cadastro? Clique aqui.</p>
             </TitleRegister>
         </div>
     )
