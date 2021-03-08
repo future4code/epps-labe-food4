9bcdbee59dddde87e58263f2413564f7fdb36f6b
fsantos22 <22fsantos@gmail.com>
2021-03-07 22:31:48 -0300

    Error Page

---
+++ src/Screens/ErrorPage/styled.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const Container=styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+
+position: fixed;
+top: 50%;
+left: 50%;
+transform: translate(-50%, -50%);
+`
+
+export const LogoImg = styled.img`
+  width: 150px;
+  height: 92px;
+  margin: 68px 128px 16px;
+`;
